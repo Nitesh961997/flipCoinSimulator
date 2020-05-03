af090ae3e1b396c42eca7a89cd86c402642a2ed6
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-03 18:57:33 +0530

    The Program Now Counts Number of Heads or Tails Wins

--- coinSimulator.sh
+++ coinSimulator.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -x
 heads=0 
-tails=1 
-result=$((RANDOM%2))
-if [ $result -eq 1 ]
-	then 
-		echo tails is winner
-	else
-		echo heads is winner
-fi
+tails=0 
+for (( toss=1; toss<=100; toss++ ))
+	do
+		result=$((RANDOM%2))
+		if [ $result -eq 1 ]
+			then 
+				echo tails is winner
+				((tails++))
+		else
+				echo heads is winner
+				((heads++))
+		fi
+	done
+echo tails wins-$heads
+echo heads wins-$tails
